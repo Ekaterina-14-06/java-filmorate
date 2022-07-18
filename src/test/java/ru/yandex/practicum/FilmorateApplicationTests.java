74dc60b14e75602bf1814c3a3788f0478575d76a
Ekaterina <lyadovakatya@yandex.ru>
2022-07-18 22:20:23 +0300

    спринт 11 тесты

--- src/test/java/ru/yandex/practicum/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/FilmorateApplicationTests.java
@@ -1,7 +1,12 @@
 package ru.yandex.practicum;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.yandex.practicum.model.Film;
+import ru.yandex.practicum.service.FilmDbService;
+import ru.yandex.practicum.storage.film.FilmDbStorage;
 
 @SpringBootTest
 class FilmorateApplicationTests {
@@ -9,5 +14,4 @@ class FilmorateApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
